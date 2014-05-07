cc1dc03b95b4ec4300d35ed630ff1a0c68abae38
zhongwuzw <zhongwuzw@qq.com>
2014-05-07 11:48:47 +0800

    外键相互访问测试

--- JobSpider/test1.py
+++ JobSpider/test1.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
 from JobSpider.models import WordLocation,WordList
-p = WordLocation.objects.filter(word_id_id = u'的')
-print p
+b = WordLocation.objects.get(pk = 1)
+print b.word_id.word
+p = WordList.objects.get(word = u'家')
+print p.wordlocation_set.all()
